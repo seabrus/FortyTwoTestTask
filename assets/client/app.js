81d7b31fc02151f8b5ae1a2eb52d9e23c92e3e78
seabrus <seabrus@gmail.com>
2016-01-13 16:11:24 +0200

    ticket:1 - Check code styling

--- assets/client/app.js
+++ assets/client/app.js
@@ -2,38 +2,42 @@ var app42 = angular.module('test42cc', ['ngCookies', 'ngRoute', 'ngSanitize']);
 
 
 // ====================================================================
-//     Initialization
+//     Set support for django's {{ csrf_token }} security mechanism
 // ====================================================================
 app42.config( ['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 }]);
 
-app42.config(['$routeProvider', '$locationProvider', function( $routeProvider, $locationProvider ) {
-  $routeProvider.when('/', { 
-    templateUrl: '/static/hello/html/home.html', 
-    controller: 'HomeController',
-    controllerAs: 'ctrl',
-    routeName: 'home',
-    resolve: {
-        isLoggedIn: ['$location', 'userState', function($location, userState) {
-            if (userState.username !== '') {
-                $location.path('/tasks');
-                $location.replace();
-            }
-        }]
-    },
-  });
+// ====================================================================
+//     Routes configuration
+// ====================================================================
+app42.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.when('/', { 
+        templateUrl: '/static/hello/html/home.html', 
+        controller: 'HomeController',
+        controllerAs: 'ctrl',
+        routeName: 'home',
+        resolve: {
+            isLoggedIn: ['$location', 'userState', function($location, userState) {
+                if (userState.username !== '') {
+                    $location.path('/tasks');
+                    $location.replace();
+                }
+            }]
+        },
+    });
 
-  $routeProvider.when('/tasks', { 
-    templateUrl: '/static/hello/html/dummy.html', 
-    controller: 'DummyController',
-    routeName: 'dummy',
-  });
+    // This is a dummy route for testing
+    $routeProvider.when('/tasks', { 
+        templateUrl: '/static/hello/html/dummy.html', 
+        controller: 'DummyController',
+        routeName: 'dummy',
+    });
 
-  $routeProvider.otherwise({ redirectTo: '/' });
+    $routeProvider.otherwise({redirectTo: '/'});
 
-  // $locationProvider.html5Mode(true); 
+    // $locationProvider.html5Mode(true); 
 }]);
 
 
@@ -51,6 +55,3 @@ app42.controller('MenuController', ['$location', 'userState', function($location
         $location.path('/tasks');
    };
 }]);
-
-
-
